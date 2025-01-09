ed83e6cdfa7002b9645808bbaf196adaa5024050
Muhammad Hamza <muhammadhamza14210@gmail.com>
2025-01-08 19:52:38 -0700

    made apis for event image and working on event form

--- src/app/seller/new-event/page.tsx
+++ src/app/seller/new-event/page.tsx
@@ -6,14 +6,14 @@ const NewEventPage = () => {
     <div className="max-w-3xl mx-auto p-6">
       <div className="bg-white rounded-lg shadow-lg overflow-hidden">
         <div className="bg-gradient-to-r from-blue-600 to-blue-800 px-8 py-6">
-          <h2 className="text-2xl font-bold">Create New Event</h2>
+          <h2 className="text-2xl font-bold text-white">Create New Event</h2>
           <p className="text-blue-100 mt-2">
             {" "}
             List your event and start selling tickets
           </p>
         </div>
         <div className="p-6">
-            <EventForm mode='create' />
+          <EventForm mode="create" />
         </div>
       </div>
     </div>
--- src/components/EventForm.tsx
+++ src/components/EventForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useTransition } from "react";
+import React, { useRef, useState, useTransition } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -10,6 +10,15 @@ import { api } from "../../convex/_generated/api";
 import { useRouter } from "next/navigation";
 import { useToast } from "@/hooks/use-toast";
 import { getStoreUrl } from "@/lib/utils";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "./ui/form";
+import { Input } from "./ui/input";
 
 const formScheme = z.object({
   name: z.string().min(1, "Event name is required"),
@@ -20,7 +29,7 @@ const formScheme = z.object({
   totalTickets: z.number().min(1, "Total tickets is required")
 });
 
-type formData = z.infer<typeof formScheme>;
+type FormData = z.infer<typeof formScheme>;
 
 interface InitialEventData {
   _id: Id<"events">;
@@ -43,9 +52,165 @@ const EventForm = ({ mode, initialData }: EventFormProps) => {
   const updateEvent = useMutation(api.events.updateEvent);
   const router = useRouter();
   const { toast } = useToast();
-  const [isPending, startTransition ] = useTransition()
+  const [isPending, startTransition] = useTransition();
   const currentImageUrl = getStoreUrl(initialData?.imageStorageUrl);
-  return <div>EventForm</div>;
+
+  //Image Upload
+  const imageInput = useRef<HTMLInputElement>(null);
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+  const [imagePreview, setImagePreview] = useState<string | null>(null);
+  const generateUploadUrl = useMutation(api.storage.generateUploadUrl);
+  const updateEventImage = useMutation(api.storage.updateEventImage);
+  const deleteImage = useMutation(api.storage.deleteImage);
+  const [removedCurrentImage, setRemovedCurrentImage] = useState(false);
+
+  const form = useForm<FormData>({
+    resolver: zodResolver(formScheme),
+    defaultValues: {
+      name: initialData?.name ?? "",
+      description: initialData?.description ?? "",
+      location: initialData?.location ?? "",
+      eventDate: initialData ? new Date(initialData?.eventDate) : new Date(),
+      price: initialData?.price ?? 0,
+      totalTickets: initialData?.totalTickets ?? 1
+    }
+  });
+
+  const onSubmit = async (data: FormData) => {};
+
+  async function handleImageUploadUrl(file: File): Promise<string | null> {
+    try {
+      const postUrl = await generateUploadUrl();
+      const result = await fetch(postUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": file.type
+        },
+        body: file
+      });
+      const { storageId } = await result.json();
+      return storageId;
+    } catch (error) {
+      console.error("Error generating upload URL:", error);
+      return null;
+    }
+  }
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setSelectedImage(file);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreview(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        {/* Form fields */}
+        <div className="space-y-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Event Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Welcome Back Dinner 2025" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Event Description</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="This is a Welcome Back Dinner for Winter 2025"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="location"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Event Location</FormLabel>
+                <FormControl>
+                  <Input placeholder="Edmonton" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="eventDate"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Event Name</FormLabel>
+                <FormControl>
+                  <Input
+                    type="date"
+                    {...field}
+                    onChange={(e) => {
+                      field.onChange(
+                        e.target.value ? new Date(e.target.value) : null
+                      );
+                    }}
+                    value={
+                      field.value
+                        ? new Date(field.value).toISOString().split("T")[0]
+                        : ""
+                    }
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="price"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Price Per Ticket</FormLabel>
+                <FormControl>
+                  <div className="relative">
+                    <span className="absolute left-2 top-1/2 transform -translate-y-1/2">
+                      $
+                    </span>
+                    <Input
+                      type="number"
+                      className="pl-6"
+                      {...field}
+                      onChange={(e) => field.onChange(Number(e.target.value))}
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+      </form>
+    </Form>
+  );
 };
 
 export default EventForm;
